44bde01b28b4034685fe78e99278f6c71a169583
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-05 14:36:00 -0800

    Adds the ability to edit reviews. It is a little wonky currently but functions.

--- my-simple-frontend/src/components/ReviewsContainer.js
+++ my-simple-frontend/src/components/ReviewsContainer.js
@@ -4,6 +4,48 @@ import ReviewForm from "./ReviewForm";
 function ReviewsContainer({reviews, id}){
 
   const[allReviews, setAllReviews] = useState(reviews)
+  const [isEditMode, setIsEditMode] = useState(true);
+  const [formData, setFormData] = useState();
+
+  function handleEditMode(){
+    setIsEditMode(!isEditMode)
+  }
+
+  function onEditReview(updatedReview){
+    const updatedReviews = allReviews.map((review) => {
+      if (review.id === updatedReview.id) {
+        return updatedReview;
+      } else {
+        return review;
+      }
+    });
+    setAllReviews(updatedReviews);
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setFormData(value)
+  }
+
+  function handleEditReviewClick(e) {
+    fetch(`http://localhost:9292/reviews/${e.currentTarget.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+          comments: formData
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItems) => {
+      onEditReview(updatedItems)
+    });
+    setFormData("")
+    handleEditMode()
+    alert("Edit complete.")
+  }
 
   function onAddReview(newReview){
     if (allReviews === undefined){
@@ -45,7 +87,12 @@ if (allReviews !== undefined){
     <ul className='review-list'>
       {allReviews.map((review) => {
         return(
-          <li id={review.id} key={review.id}>Anon says: "{review.comments}", rating it a {review.user_rating} out of 5 overall and a scare factor of {review.scare_scale}!<button id={review.id} type='submit' onClick={handleDeleteReview}>Delete The Review</button></li>
+          <div key={review.id}>
+          
+          {isEditMode? <li id={review.id} key={review.id}>Anon says: "{review.comments}", rating it a {review.user_rating} out of 5 overall and a scare factor of {review.scare_scale}!<button id={review.id} type='submit' onClick={handleDeleteReview}>Delete The Review</button><button id='edit-review-button' type='submit'onClick={handleEditMode}>{isEditMode? 'edit?': 'nevermind!'}</button></li> : <div><input onChange={handleChange} type='text' placeholder={review.comments}></input><button button id={review.id} type='submit' onClick={handleEditReviewClick}>Submit Update</button></div>}
+      
+          
+          </div>
         )
       })}
     </ul>
