5a4be0bab9a019496cb5b3925fd442ee9e703dc6
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-04 19:08:35 -0800

    Creates a review component and the const setting the review which will be passed down as props.

--- my-simple-frontend/src/components/MoviePage.js
+++ my-simple-frontend/src/components/MoviePage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Reviews from "./ReviewsContainer";
 
 function MoviePage({movies}){
 
   const {movieId} = useParams();
   const movie = movies[movieId -1] //minus one to account for the array offset
+  const reviews = movie.reviews
+  console.log(reviews)
 
 return (
   <div className="page" id={movie.id}>
@@ -14,6 +17,9 @@ return (
       <h3>Directed by: {movie.director}</h3> 
       <p>{movie.title} is a {movie.subgenre} horror movie, originally released in {movie.year}</p>
       <p>Synopsis: {movie.description}</p>
+
+      <h1>Reviews:</h1>
+      <Reviews />
     </div>
 )
 
---
+++ my-simple-frontend/src/components/ReviewsContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Reviews(){
+
+  return(
+
+    <h3> Read the reviews.</h3>
+  )
+}
+
+export default Reviews;
