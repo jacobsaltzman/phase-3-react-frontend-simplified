a856ec89fc772aeddfd0466b7df4cd91cc5c8481
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-04 19:41:37 -0800

    Adds the reviews to the individual movie rendering, mapping through them and displaying them all or that there are none.

--- my-simple-frontend/src/components/ReviewsContainer.js
+++ my-simple-frontend/src/components/ReviewsContainer.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React from 'react';
 
-function ReviewsContainer(){
+function ReviewsContainer({reviews}){
 
+
+if (reviews !== undefined){
+  
+  
   return(
-
-    <h3> Read the reviews.</h3>
+    <div>
+    <h3>There are {reviews.length} reviews for this movie!</h3>
+    <ul className='review-list'>
+      {reviews.map((review) => {
+        return(
+          <li id={review.id} key={review.id}>Anon says: "{review.comments}", rating it a {review.user_rating} out of 5 overall and a scare factor of {review.scare_scale}!</li>
+        )
+      })}
+    </ul>
+    </div>
   )
+
+}
+  else{
+    return(
+      <p>No Reviews Yet...</p>
+    )
+  }
 }
 
 export default ReviewsContainer;
